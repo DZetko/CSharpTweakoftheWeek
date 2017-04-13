0813b486ecdb5ac07290169c1200afa94a4c3062
Daniel Zikmund <Daniel Zikmund>
2017-04-13 23:40:48 +0200

    Add code for Type Indexers video on my channel
    Signed-off-by: Daniel Zikmund <Daniel Zikmund>

---
+++ TypeIndexers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeIndexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserList userList = new UserList();
+
+            userList.Add(new User(1, "Daniel"));
+            userList.Add(new User(2, "Martin"));
+            userList.Add(new User(3, "Stewie"));
+
+            var user = userList["Stewie"];
+
+            Console.WriteLine($"Name: {user.Name}, id: {user.Id}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TypeIndexers/UserList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeIndexers
+{
+    public class UserList : List<User>
+    {
+        public User this[string name]
+        {
+            get
+            {
+                foreach (User user in this)
+                {
+                    if (user.Name == name)
+                    {
+                        return user;
+                    }
+                }
+
+                return null;
+            }
+        }
+    }
+}
